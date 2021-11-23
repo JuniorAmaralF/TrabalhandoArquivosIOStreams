d550540e5b1b0163726ac3ab841b9909edafa5d7
JuniorAmaralF <85956913+JuniorAmaralF@users.noreply.github.com>
2021-11-23 19:09:39 -0300

    Lendo zip com senha

--- leitor-zip.php
+++ leitor-zip.php
@@ -1,3 +1,20 @@
 <?php
 //# especifica qual arquivo voce que ler dentro do zip
-echo file_get_contents('zip://arquivos.zip#lista-cursos.txt');
+//echo file_get_contents('zip://arquivos.zip#lista-cursos.txt');
+
+
+//criando um contexto para o protocolo zip, para poder ler o arquivo com senha
+
+$contexto = stream_context_create([
+    'zip' => [
+        'password' => '123456'
+    ]
+    ]);
+
+//# especifica qual arquivo voce que ler dentro do zip
+echo file_get_contents(
+    'zip://arquivosSenha.zip#lista-cursos.txt',
+    false,
+    $contexto
+);
+
